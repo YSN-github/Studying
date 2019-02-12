a0de5282ee22f7225c277d97f27f140005b2fbcc
kazuk <kazuk@DESKTOP-GBHT4D8>
2019-02-12 16:09:06 +0900

    commit

--- test/learn/Basic.java
+++ test/learn/Basic.java
@@ -3,33 +3,47 @@ package learn;
 public class Basic{
     public static void main(String args[]){//UTF-8
 
+    line("文字の表示");
     System.out.println("Hallo world");
+    end();
+
+    line("変数作成");
     String a="YSN_JP";
     System.out.println("a="+a);
     int b= 200+300;
     System.out.println("b="+b);
+    end();
 
+    line("配列");
     int c[]=new int [5];
     c[0]=10;
     c[1]=20;
     c[2]=30;
     System.out.println("c="+(c[0]+c[1]+c[2]));
+    end();
 
+    line("二次元配列(長さ指定)");
     int d[][]=new int[2][2];
     d[0][0]=10;
     d[0][1]=20;
     d[1][0]=30;
     d[1][1]=40;
     System.out.println("d="+(d[0][0]+d[1][1]));
-
+    line("二次元配列（データ指定）");
     int e[][] = {{2,4},{6,8},{10}};
     System.out.println("e="+(e[0][1]+e[2][0]));
     System.out.println("e2="+e.length+" "+e[0].length+" "+e[1].length);
+    end();
 
+    line("演算子(+=等にて計算)");
     int f=30;
-    System.out.print("f=");
-    System.out.println(f+=70);
-    System.out.println("f2="+f);
+    System.out.println("f="+f);
+    System.out.println("f+70→"+(f+=70));
+    System.out.println("f-30→"+(f-=30));
+    System.out.println("f*10→"+(f*=10));
+    System.out.println("f/3→"+(f/=3));
+    System.out.println("f%4→"+(f%=4));
+    end();
 
     int g =30;
     System.out.print("g=");
@@ -76,8 +90,14 @@ public class Basic{
        System.out.println(" int l = " + n);
        System.out.println(" int m = " + o);
     }
-
-
-
-
-  }
+    static void line(String a) {
+    	System.out.println("------------------------------------------");
+    	System.out.println("a");
+    	System.out.println("");
+    }
+    static void end() {
+    	System.out.println("");
+    	System.out.println("a");
+    	System.out.println("------------------------------------------");
+    }
+}
